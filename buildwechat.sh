7553722621313207a7735603f1c7bb02db897df5
lin <120377843@qq.com>
2021-09-06 09:39:37 +0800

    add new one article

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,11 +1,11 @@
-folder=thinking
-fileName=past-dr-framework
-imageHost=https://github.com/shaogefenhao/wiki/raw/master/docs/_posts/$folder/
+folder=01.建模/02.ddd
+fileName=15.why-ddd-works
+imageHost=https://github.com/shaogefenhao/wiki/raw/master/docs/$folder/
 
 rm -rf build
 mkdir build
 
-cat docs/_posts/${folder}/${fileName}.md > build/output.md
+cat docs/${folder}/${fileName}.md > build/output.md
 sed -i "" "s@$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
 pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
